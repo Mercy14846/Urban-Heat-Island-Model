de2835dccc63c6efa8c07e9fa3e7f0307df3ae16
Mercy14846 <akintolamercy21@gmail.com>
2024-10-21 16:29:50 +0100

    Update test size in train_test_split

--- model.py
+++ model.py
@@ -30,7 +30,7 @@ def develop_model(data):
     X = data[['urban_temp', 'rural_temp', 'humidity', 'wind_speed']]
     y = data['temp_diff']
     
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
     
     scaler = StandardScaler()
     X_train = scaler.fit_transform(X_train)
